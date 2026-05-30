e2819fd21cc6dd3079c24d651e65dfbc8e127c2f
Matejda1 <matejka.patri@gmail.com>
2026-05-30 14:51:19 +0200

    Complete Website
    End of editing contact section. Make full footer. Ready for demo deploy for full deploy need add foto.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
-const hamburger = document.querySelector(".hamburger")
-const navbar = document.querySelector(".navbar-links")
-let menuOpen = false
+// Navbavr
+const hamburger = document.querySelector(".hamburger");
+const navbar = document.querySelector(".navbar-links");
+let menuOpen = false;
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("change")
@@ -12,3 +13,71 @@ hamburger.addEventListener("click", () => {
         menuOpen = false
     }
 })
+
+// Form validation
+document.querySelectorAll('.contact-form input, .contact-form textarea').forEach(input => {
+    input.addEventListener('blur', () => {
+        input.classList.add('touched');
+    });
+});
+
+const submitBtn = document.querySelector("#submitBtn")
+
+document.addEventListener("DOMContentLoaded", function () {
+    const formular = document.getElementById("contact-form");
+    const inputN = document.getElementById("name-input");
+    const inputE = document.getElementById("email-input");
+    const inputM = document.getElementById("message-input");
+
+    if (formular) {
+        formular.addEventListener("submit", function (e) {
+            e.preventDefault();
+            submitBtn.style.background = "#D3D3D3"
+
+            const scriptURL = "https://script.google.com/macros/s/AKfycbztQ-FtQQVU8nVSFW3nw4bWiUqEK07-Dh3P06Pc-l7htxsN2pTFbWl1QO2HwCTyJEe8/exec";
+
+            const formData = new FormData(formular);
+            fetch(scriptURL, { method: "POST", body: formData })
+                .then(response => {
+                    if (response.ok) {
+                        showAlert("Zpráva uspěšně odeslána!", "Vaše zpráva se uspěšně odeslala. Zachvilku se na ni podívam.", "Zavřít");
+                        formular.reset();
+                        inputN.classList.remove("touched");
+                        inputE.classList.remove("touched")
+                        inputM.classList.remove("touched")
+                    } else {
+                        showAlert("Něco se nepovedlo!", "Zkuste prosím zprávu poslat znovu.", "Zavřít");
+                    }
+                })
+                .catch(error => {
+                    console.error("Chyba při odesílání:", error);
+                    showAlert("Chyba sítě!", "Zprávu nebylo možné odeslat. Zkontrolujte připojení.", "Zavřít");
+                });
+        });
+    }
+});
+
+const alertBox = document.querySelector(".alert");
+const titleA = document.querySelector(".titleA");
+const textA = document.querySelector(".text");
+const confirmABtn = document.querySelector(".confirmABtn");
+
+function showAlert(title, text, btnText) {
+    titleA.innerHTML = title
+    textA.innerHTML = text;
+    confirmABtn.innerHTML = btnText;
+
+    alertBox.style.display = "flex";
+}
+
+confirmABtn.addEventListener("click", () => {
+    alertBox.style.display = "none";
+    titleA.innerHTML = "";
+    textA.innerHTML = "";
+    confirmABtn.innerHTML = "";
+});
+
+// Footer
+let year = new Date().getFullYear();
+const footerYear = document.querySelector("#footer-year");
+footerYear.innerHTML = year;
